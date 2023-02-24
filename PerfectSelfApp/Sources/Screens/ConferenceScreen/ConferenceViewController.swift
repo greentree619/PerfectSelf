7d8e2ebc3e699a0510ed55b15c1739690e59e543
GreenFullStack <greentree619@outlook.com>
2023-02-23 21:40:35 -0500

    Add meetinglist and conference UI

---
+++ PerfectSelfApp/Sources/Screens/ConferenceScreen/ConferenceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MeetingListViewController.swift
+//  PerfectSelf
+//
+//  Created by Kayan Mishra on 2/23/23.
+//  Copyright © 2023 Stas Seldin. All rights reserved.
+//
+
+import SwiftUI
+import AVFoundation
+import WebRTC
+
+class ConferenceViewController: UIViewController {
+
+   
+}
