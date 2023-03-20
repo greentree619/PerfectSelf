8d61be6e1805719e69b2eb5e1900d770b35ba1f0
tonybingo-stack <tonybingo19970425@outlook.com>
2023-03-20 18:25:50 -0400

    Actor booking screen completed

--- PerfectSelfApp/Sources/Screens/ActorBookingScreen/ActorBookingViewController.swift
+++ PerfectSelfApp/Sources/Screens/ActorBookingScreen/ActorBookingViewController.swift
@@ -11,40 +11,93 @@ import UIKit
 class ActorBookingViewController: UIViewController {
 
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var btn_upcoming: UIButton!
+    @IBOutlet weak var btn_past: UIButton!
+    @IBOutlet weak var btn_pending: UIButton!
     
+    @IBOutlet weak var line_upcoming: UIImageView!
+    @IBOutlet weak var line_pending: UIImageView!
+    @IBOutlet weak var line_past: UIImageView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    let r = UIImage(named: "book");
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let containerView = UIView()
+        line_pending.isHidden = true
+        line_past.isHidden = true
         
+        let containerView = UIView()
         let num = 0...10
         for i in num {
-  
-            let item = Item(frame: CGRect(x: 20, y:120*i, width:Int(scrollView.frame.width-40), height:100), labelText: "Booking History")
-            item.tapCallback = {
-                let controller = ActorReaderDetailViewController();
-                
-                self.navigationController?.pushViewController(controller, animated: true)
-            }
-            
-            item.layer.masksToBounds = false;
-            item.layer.shadowOpacity = 0.5;
-            item.layer.shadowRadius = 5;
-            item.layer.shadowOffset = CGSize(width: 2, height: 5);
-            item.layer.cornerRadius = 10
-            
-            containerView.addSubview(item)
+            let iv = UIImageView()
+            iv.image = r;
+            iv.layer.masksToBounds = false;
+            iv.layer.shadowOpacity = 0.3;
+            iv.layer.shadowRadius = 3;
+            iv.layer.shadowOffset = CGSize(width: 2, height: 3);
+            iv.frame = CGRect(x: 0, y:120*i, width:Int(scrollView.frame.width), height:100)
+            containerView.addSubview(iv)
         }
    
-        containerView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.width, height: 1310)
+        containerView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.width, height: 800)
         
         scrollView.addSubview(containerView)
         scrollView.contentSize = containerView.frame.size
+//        let containerView = UIView()
+//
+//        let num = 0...10
+//        for i in num {
+//
+//            let item = Item(frame: CGRect(x: 0, y:120*i, width:Int(scrollView.frame.width), height:100), labelText: "Booking History")
+//            item.tapCallback = {
+//                let controller = ActorReaderDetailViewController();
+//
+//                self.navigationController?.pushViewController(controller, animated: true)
+//            }
+//
+//            item.layer.masksToBounds = false;
+//            item.layer.shadowOpacity = 0.5;
+//            item.layer.shadowRadius = 5;
+//            item.layer.shadowOffset = CGSize(width: 2, height: 5);
+//            item.layer.cornerRadius = 10
+//
+//            containerView.addSubview(item)
+//        }
+//
+//        containerView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.width, height: 1310)
+//
+//        scrollView.addSubview(containerView)
+//        scrollView.contentSize = containerView.frame.size
     }
 
-
+    @IBAction func ShowUpcomingBookings(_ sender: UIButton) {
+        sender.tintColor = UIColor(rgb: 0x4063FF)
+        btn_pending.tintColor = .black
+        btn_past.tintColor = .black
+        line_upcoming.isHidden = false
+        line_pending.isHidden = true
+        line_past.isHidden = true
+    }
+    
+    @IBAction func ShowPendingBookings(_ sender: UIButton) {
+        sender.tintColor = UIColor(rgb: 0x4063FF)
+        btn_upcoming.tintColor = .black
+        btn_past.tintColor = .black
+        line_upcoming.isHidden = true
+        line_pending.isHidden = false
+        line_past.isHidden = true
+    }
+    
+    @IBAction func ShowPastBookings(_ sender: UIButton) {
+        sender.tintColor = UIColor(rgb: 0x4063FF)
+        btn_upcoming.tintColor = .black
+        btn_pending.tintColor = .black
+        line_upcoming.isHidden = true
+        line_pending.isHidden = true
+        line_past.isHidden = false
+    }
     /*
     // MARK: - Navigation
 
