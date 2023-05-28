ab2e0012fb496a9b923241b047b1d26270c842f5
GreenFullStack <greentree619@outlook.com>
2023-05-28 08:14:30 -0700

    version up 2.6.57

--- PerfectSelfApp/Sources/Screens/ConferenceScreen/ConferenceViewController.swift
+++ PerfectSelfApp/Sources/Screens/ConferenceScreen/ConferenceViewController.swift
@@ -236,27 +236,27 @@ class ConferenceViewController: UIViewController, AVCaptureVideoDataOutputSample
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        DispatchQueue.main.async {
-            self.count = self.selectedCount
-            self.lblTimer.text = "\(self.count)"
-            self.lblTimer.isHidden = false
-            if self.timer != nil {
-                self.timer.invalidate()
-            }
-            
-            self.btnBack.isUserInteractionEnabled = false
-            self.btnLeave.isUserInteractionEnabled = false
-            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
-                self.count -= 1
-                self.lblTimer.text = "\(self.count)"
-                if self.count == 0 {
-                    self.lblTimer.isHidden = true
-                    timer.invalidate()
-                    self._captureState = .start
-                    self.audioRecorder?.record()
-                }
-            })
-        }
+//        DispatchQueue.main.async {
+//            self.count = self.selectedCount
+//            self.lblTimer.text = "\(self.count)"
+//            self.lblTimer.isHidden = false
+//            if self.timer != nil {
+//                self.timer.invalidate()
+//            }
+//            
+//            self.btnBack.isUserInteractionEnabled = false
+//            self.btnLeave.isUserInteractionEnabled = false
+//            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+//                self.count -= 1
+//                self.lblTimer.text = "\(self.count)"
+//                if self.count == 0 {
+//                    self.lblTimer.isHidden = true
+//                    timer.invalidate()
+//                    self._captureState = .start
+//                    self.audioRecorder?.record()
+//                }
+//            })
+//        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
