d02257c15ccee4d309624a7ba42273973d293d80
Green Full Stack <greentree619@outlook.com>
2023-07-05 04:11:04 -0700

    Add to select microphone on Overlay Page

---
+++ PerfectSelfApp/Sources/Screens/OverlayScreen/AudioInputTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioInputCell.swift
+//  PerfectSelf
+//
+//  Created by user237184 on 7/4/23.
+//  Copyright © 2023 Stas Seldin. All rights reserved.
+//
+
+import UIKit
+
+class AudioInputTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ PerfectSelfApp/Sources/Screens/OverlayScreen/MicrophoneListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MicrophoneListViewController.swift
+//  PerfectSelf
+//
+//  Created by user237184 on 7/4/23.
+//  Copyright © 2023 Stas Seldin. All rights reserved.
+//
+
+import UIKit
+
+class MicrophoneListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
