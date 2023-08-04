652b6cd66da56fd38d3cb3aa1fa614acfbf3e1f7
tonybingo-stack <tonybingo19970425@outlook.com>
2023-08-04 00:48:05 -0700

    Updated Folder main logic.

---
+++ PerfectSelfApp/Sources/Screens/ActorLibraryScreen/TapeItemViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TapeItemViewCell.swift
+//  PerfectSelf
+//
+//  Created by user237181 on 8/3/23.
+//  Copyright © 2023 Stas Seldin. All rights reserved.
+//
+
+import UIKit
+
+class TapeItemViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
